cc6c705bdb6167e673d51eb581fd16acbe63c6fa
Brad Bell <bradbell@seanet.com>
2021-08-08 17:31:17 -0700

    master: plan.xsrst: change start_node->root_node, end_node_set->leaf_node_set.
    level2_leaf4.py: Under Construction.

---
+++ example/level2_leaf4.py
@@ -0,0 +1,31 @@
+# -----------------------------------------------------------------------------
+# at_cascade: Cascading Dismod_at Analysis From Parent To Child Regions
+#           Copyright (C) 2021-21 University of Washington
+#              (Bradley M. Bell bradbell@uw.edu)
+#
+# This program is distributed under the terms of the
+#     GNU Affero General Public License version 3.0 or later
+# see http://www.gnu.org/licenses/agpl.txt
+# -----------------------------------------------------------------------------
+'''
+{xsrst_begin xam_level2_leaf4}
+
+A Simple Example with Two Levels and Four Leaf Nodes
+====================================================
+
+Node Table
+**********
+The following is a diagram of the node tree for this example::
+
+                n1
+          /-----/\-----\
+        n11            n12
+      /     \        /     \
+    n111   n112    n121   n122
+
+We refer to n1 as the *start_node* and
+n111, n112, n121, n122 as the leaf nodes.
+
+
+{xsrst_end xam_level2_leaf4}
+'''
