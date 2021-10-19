42034bd879aa692c6257edf93687683a6fd1f5da
Brad Bell <bradbell@seanet.com>
2021-10-19 05:34:37 -0700

    master: data4cov_reference.py: alter all_cov_reference table (not covariate table).

--- at_cascade/data4cov_reference.py
+++ at_cascade/data4cov_reference.py
@@ -20,44 +20,66 @@ Syntax
     # END syntax
 }
 
-connection
-**********
-This is a dismod_at connection to a dismod_at database.
-Only the following tables are required:
+root_node_database
+*******************
+This argument can't be ``None`` and
+is the :ref:`glossary.root_node_database`.
+This database is not modified and
+only following tables are used:
 option, data, node, covariate.
 
 option Table
-************
+============
 This table must have a row with option_value equal to parent_node_name.
 Only this row of the option table is used.
 
 data Table
-**********
+==========
 Only the following columns of this table are used:
 data_id, node_id, x_0, ... x_n,
 where n is the number of covariates minus one.
 
-covariate_table
+all_node_database
+*****************
+This argument can't be ``None`` and is the :ref:all_node_db`.
+Only the :ref:`all_cov_reference_table`
+and :ref:`all_option_table` are used.
+
+all_option Table
 ================
-The ``reference`` column in the covariate table is replaced
-by the average of the corresponding covariate in the data table.
+The :ref:`all_option_table.split_list` row of this table
+(if it exists) is the only row of this table that is used.
+
+all_cov_reference Table
+=======================
+The :ref:`all_cov_reference_table.reference` column for some of the rows in
+this table is replaced by the average of the corresponding covariate in the
+data table.
 Only the rows of the data table that satisfy the following conditions
-are included:
+are included when computing the average:
 
-1.  The node for the data table row is the parent node or a descendant of
-    the parent node.
+1.  The node_id for the data table row is the
+    :ref:`all_cov_reference_table.node_id` for the all_cov_reference row,
+    or a descendant of all_cov_reference node.
 
-2.  The difference of each covariate value in the data table row
+2.  The difference of all the covariate values in the data table row
     from the corresponding reference in the covariate table is
     less than or equal the maximum difference for that covariate.
 
-Only the reference values for covariates that satisfy the following conditions
-are modified:
+Only the rows of the all_cov_reference table
+that satisfy the following conditions are modified:
 
 3.  There is at least one row of the data table that satisfies conditions
-    1 and 2 for this covariate.
+    1 and 2 for this all_cov_reference
+    :ref:`all_cov_reference_table.covariate_id`.
 
-4.  This covariate has maximum difference equal to infinity (or null).
+4.  This all_cov_reference covariate has maximum difference equal to
+    infinity (or null).
+
+5.  The :ref:`all_cov_reference_table.split_reference_id` for this row
+    corresponds to the covariate value for splitting covariate in the
+    covariate table.
+    (2DO: This should be extended to all values for the splitting covariate.)
 
 {xsrst_end data4cov_reference}}
 '''
@@ -65,101 +87,155 @@ import at_cascade
 import dismod_at
 import math
 #
+# data4cov_reference(
+def data4cov_reference(
 # BEGIN syntax
-# data4cov_reference(database)
+    all_node_database  = None,
+    root_node_database = None
 # END syntax
-def data4cov_reference(connection) :
+) :
     #
-    # tables
-    tables = dict()
+    # all_table
+    new        = False
+    connection = dismod_at.create_connection(all_node_database, new)
+    all_table  = dict()
+    for tbl_name in [
+        'all_option',
+        'all_cov_reference',
+    ] :
+        all_table[tbl_name] = dismod_at.get_table_dict(connection, tbl_name)
+    connection.close()
+    #
+    # root_table
+    new        = False
+    connection = dismod_at.create_connection(root_node_database, new)
+    root_table = dict()
     for tbl_name in [
         'option',
         'data',
         'node',
         'covariate',
     ] :
-        tables[tbl_name] = dismod_at.get_table_dict(connection, tbl_name)
+        root_table[tbl_name] = dismod_at.get_table_dict(connection, tbl_name)
+    connection.close()
     #
-    # parent_name_id
-    parent_node_id = None
-    for row in tables['option'] :
-        if row['option_name'] == 'parent_node_name' :
-            parent_node_name = row['option_value']
-            parent_node_id   = at_cascade.table_name2id(
-                tables['node'], 'node', parent_node_name
+    # split_reference_id
+    split_reference_id = None
+    for row in all_table['all_option'] :
+        if row['option_name'] == 'split_list' :
+            split_list = row['option_value'].split()
+            split_covariate_name = split_list[1]
+            split_covariate_id   = at_cascade.table_name2id(
+                root_table['covariate'], 'covariate', split_covariate_name
             )
-    assert not parent_node_id is None
-    #
-    # is_decendant
-    is_descendant = list()
-    for (node_id, row) in enumerate(tables['node']) :
-        this_is_descendant = node_id == parent_node_id
-        ancestor_node_id   = row['parent']
-        while not ancestor_node_id is None :
-            if ancestor_node_id == parent_node_id :
-                this_is_descendant = True
-            ancestor_node_id = tables['node'][ancestor_node_id]['parent']
-        is_descendant.append( this_is_descendant)
+            split_reference_list = split_list[2:]
+            for (k, reference) in enumerate(split_reference_list) :
+                split_reference_list[k] = float( reference )
+            reference = root_table['covariate'][split_covariate_id]['reference']
+            if not reference in split_reference_list :
+                msg  = 'The reference value for the splitting covariate '
+                msg += '{split_covariate_name}\n'
+                msg += 'is not in the split_reference_list = '
+                msg += f'{split_reference_list}'
+                assert False, msg
+            split_reference_id = split_reference_list.index(reference)
     #
     # n_covariate
-    n_covariate = len( tables['covariate'] )
+    n_covariate = len( root_table['covariate'] )
     #
     # covariate_label
     covariate_label = list()
     for covariate_id in range( n_covariate ) :
         covariate_label.append( f'x_{covariate_id}' )
     #
-    # data_subset_list
-    data_subset_list = list()
-    for (data_id, data_row) in enumerate(tables['data']) :
-        node_id = data_row['node_id']
-        if is_descendant[node_id] :
-            in_bounds = True
-            for covariate_id in range( n_covariate ) :
-                label               = covariate_label[covariate_id]
-                covariate_value     = data_row[label]
-                covariate_row       = tables['covariate'][covariate_id]
-                reference           = covariate_row['reference']
-                max_difference      = covariate_row['max_difference']
-                assert not reference is None
-                #
-                if max_difference is None :
-                    max_difference = math.inf
-                if covariate_value is None :
-                    covariate_value = reference
-                abs_difference      = abs( covariate_value - reference )
-                in_bounds = in_bounds and abs_difference <= max_difference
-            if in_bounds :
-                data_subset_list.append( data_id )
-    #
-    # covariate_value
-    covariate_value = list()
-    for covariate_id in range( n_covariate ) :
-        covariate_value.append( list() )
-    for data_id in data_subset_list :
-        data_row = tables['data'][data_id]
-        for covariate_id in range( n_covariate ) :
-            covariate_row  = tables['covariate'][covariate_id]
-            max_difference = covariate_row['max_difference']
-            cov_value      = data_row[ covariate_label[covariate_id] ]
-            if not cov_value is None :
-                if max_difference is None or max_difference == math.inf :
-                    covariate_value[covariate_id].append(cov_value)
+    # parent_name_id
+    parent_node_id = None
+    for row in root_table['option'] :
+        if row['option_name'] == 'parent_node_name' :
+            parent_node_name = row['option_value']
+            parent_node_id   = at_cascade.table_name2id(
+                root_table['node'], 'node', parent_node_name
+            )
+    assert not parent_node_id is None
     #
     # covariate_avg
     covariate_avg = list()
-    for covariate_id in range( n_covariate ) :
-        cov_list = covariate_value[covariate_id]
-        if len( cov_list ) == 0 :
-            covariate_avg.append(None)
-        else :
-            covariate_avg.append( sum(cov_list) / len(cov_list) )
+    for avg_node_id in range( len(root_table['node'] ) ) :
+        assert len( covariate_avg ) == avg_node_id
+        covariate_avg.append( None )
+        #
+        # is_decendant
+        is_descendant = list()
+        for (node_id, row) in enumerate(root_table['node']) :
+            this_is_descendant = node_id == avg_node_id
+            ancestor_node_id   = row['parent']
+            while not ancestor_node_id is None :
+                if ancestor_node_id == avg_node_id :
+                    this_is_descendant = True
+                ancestor_row     = root_table['node'][ancestor_node_id]
+                ancestor_node_id = ancestor_row['parent']
+            is_descendant.append( this_is_descendant)
+        #
+        # data_subset_list
+        data_subset_list = list()
+        for (data_id, data_row) in enumerate(root_table['data']) :
+            node_id = data_row['node_id']
+            if is_descendant[node_id] :
+                in_bounds = True
+                for covariate_id in range( n_covariate ) :
+                    label               = covariate_label[covariate_id]
+                    covariate_value     = data_row[label]
+                    covariate_row       = root_table['covariate'][covariate_id]
+                    reference           = covariate_row['reference']
+                    max_difference      = covariate_row['max_difference']
+                    assert not reference is None
+                    #
+                    if max_difference is None :
+                        max_difference = math.inf
+                    if covariate_value is None :
+                        covariate_value = reference
+                    abs_difference      = abs( covariate_value - reference )
+                    in_bounds = in_bounds and abs_difference <= max_difference
+                if in_bounds :
+                    data_subset_list.append( data_id )
+        #
+        # covariate_value
+        covariate_value = list()
+        for covariate_id in range( n_covariate ) :
+            covariate_value.append( list() )
+        for data_id in data_subset_list :
+            data_row = root_table['data'][data_id]
+            for covariate_id in range( n_covariate ) :
+                covariate_row  = root_table['covariate'][covariate_id]
+                max_difference = covariate_row['max_difference']
+                cov_value      = data_row[ covariate_label[covariate_id] ]
+                if not cov_value is None :
+                    if max_difference is None or max_difference == math.inf :
+                        covariate_value[covariate_id].append(cov_value)
+        #
+        # covariate_avg[avg_node_id]
+        covariate_avg[avg_node_id] = list()
+        for covariate_id in range( n_covariate ) :
+            cov_list = covariate_value[covariate_id]
+            if len( cov_list ) == 0 :
+                avg = None
+            else :
+                avg = sum(cov_list) / len(cov_list)
+            covariate_avg[avg_node_id].append( avg )
     #
-    # tables['covariate']
-    for (covariate_id, row) in enumerate(tables['covariate']) :
-        avg = covariate_avg[covariate_id]
-        if not avg is None :
-            row['reference'] = avg
-    dismod_at.replace_table(connection, 'covariate', tables['covariate'])
+    # all_table['all_cov_reference']
+    for row in all_table['all_cov_reference'] :
+        if row['split_reference_id'] == split_reference_id  \
+        or split_reference_id is None :
+            covariate_id = row['covariate_id']
+            node_id      = row['node_id']
+            avg          =  covariate_avg[node_id][covariate_id]
+            if not avg is None :
+                row['reference'] = avg
+    new        = False
+    connection = dismod_at.create_connection(all_node_database, new)
+    dismod_at.replace_table(
+        connection, 'all_cov_reference', all_table['all_cov_reference']
+    )
     #
     return
--- test/data4cov_reference.py
+++ test/data4cov_reference.py
@@ -28,15 +28,64 @@ def main() :
     # change into the build/test directory
     distutils.dir_util.mkpath('build/test')
     os.chdir('build/test')
-    #
-    # database
-    database  = 'root_node.db'
+    # ------------------------------------------------------------------------
+    # all_node_database
+    all_node_database = 'all_node.db'
     #
     # connection
     new        = True
-    connection = dismod_at.create_connection(database, new)
+    connection = dismod_at.create_connection(all_node_database, new)
     #
-    # option_table
+    # split_reference_list
+    split_reference_list = [ -0.5, 0.0, 0.5 ]
+    #
+    # all_option table
+    tbl_name = 'all_option'
+    col_name = [ 'option_name', 'option_value' ]
+    col_type = [ 'text',        'text'         ]
+    split_level          = '-1 '
+    split_covariate_name = 'sex '
+    split_list = split_level + split_covariate_name
+    for reference in split_reference_list :
+        split_list += ' ' + str(reference)
+    row_list = [
+        [ 'split_list', split_list ]
+    ]
+    dismod_at.create_table(
+        connection, tbl_name, col_name, col_type, row_list
+    )
+    #
+    # all_cov_reference table
+    tbl_name = 'all_cov_reference'
+    col_name = ['node_id', 'covariate_id', 'split_reference_id', 'reference']
+    col_type = ['integer', 'integer',      'integer',            'real'     ]
+    row_list = list()
+    for node_id in range(4) :
+        for (k, sex_reference)  in enumerate(split_reference_list) :
+            split_reference_id = str(k)
+            #
+            covariate_id = '0' # sex
+            row = [ node_id, covariate_id, split_reference_id, sex_reference ]
+            row_list.append(row)
+            #
+            covariate_id  = '1' # bmi
+            row = [ str(node_id), covariate_id, split_reference_id, '100.0' ]
+            row_list.append(row)
+    dismod_at.create_table(
+        connection, tbl_name, col_name, col_type, row_list
+    )
+    connection.close()
+    #
+    # ------------------------------------------------------------------------
+    # root_node_database
+    root_node_database  = 'root_node.db'
+    #
+    # connection
+    connection.close()
+    new        = True
+    connection = dismod_at.create_connection(root_node_database, new)
+    #
+    # option table
     tbl_name = 'option'
     col_name = [ 'option_name', 'option_value' ]
     col_type = [ 'text',        'text'         ]
@@ -47,7 +96,7 @@ def main() :
         connection, tbl_name, col_name, col_type, row_list
     )
     #
-    # node_table
+    # node table
     tbl_name = 'node'
     col_name = [ 'node_name', 'parent'  ]
     col_type = [ 'text',      'integer' ]
@@ -61,45 +110,64 @@ def main() :
         connection, tbl_name, col_name, col_type, row_list
     )
     #
-    # covariate_table
+    #
+    # covariate table
+    split_reference_id = 1
     tbl_name = 'covariate'
     col_name = [ 'covariate_name', 'reference', 'max_difference' ]
     col_type = [ 'text',           'real',      'real'           ]
     row_list = [
-        [ 'sex',   0.0,   0.6 ],
-        [ 'bmi',   10.0,  None ],
+        [ 'sex',   split_reference_list[split_reference_id],   0.6 ],
+        [ 'bmi',   10.0,                                       None ],
     ]
     dismod_at.create_table(
         connection, tbl_name, col_name, col_type, row_list
     )
     #
-    # data_table
+    # data table
     tbl_name = 'data'
     col_name = [ 'node_id', 'x_0',  'x_1'  ]
     col_type = [ 'integer', 'real', 'real' ]
     row_list = [
-        [ 0, 0.0, 1e4 ], # node not descendant of parent node
         [ 1, 1.0, 1e4 ], # sex does not satisfy max_difference
-        [ 1, 0.5, 1.0 ], # included in average
-        [ 2, 0.5, 2.0 ], # included in average
-        [ 3, 0.5, 3.0 ], # included in average
+        [ 0, 0.0, 0.0 ], # include in average for node_id >= 0
+        [ 1, 0.5, 1.0 ], # included in average for node_id >= 1
+        [ 2, 0.5, 2.0 ], # included in average for node_id >= 2
+        [ 3, 0.5, 3.0 ], # included in average for node_id >= 3
     ]
     dismod_at.create_table(
         connection, tbl_name, col_name, col_type, row_list
     )
+    # ------------------------------------------------------------------------
     #
     # data4cov_reference
-    at_cascade.data4cov_reference(connection)
+    at_cascade.data4cov_reference(
+        all_node_database  = all_node_database,
+        root_node_database = root_node_database
+    )
     #
-    # covariate_table
-    covariate_table = dismod_at.get_table_dict(connection, 'covariate')
+    # all_cov_reference_table
+    new             = False
+    connection      = dismod_at.create_connection(all_node_database, new)
+    all_cov_reference_table = dismod_at.get_table_dict(
+        connection, 'all_cov_reference'
+    )
     #
     # check
-    check = [
-        {'covariate_name': 'sex', 'reference': 0.0, 'max_difference': 0.6},
-        {'covariate_name': 'bmi', 'reference': 2.0, 'max_difference': None},
-    ]
-    assert covariate_table == check
+    for row in all_cov_reference_table :
+        node_id            = row['node_id']
+        covariate_id       = row['covariate_id']
+        split_reference_id = row['split_reference_id']
+        reference          = row['reference']
+        same               = node_id == 0 or split_reference_id != 1
+        if covariate_id == 0 :
+            assert reference == split_reference_list[split_reference_id]
+        else :
+            if split_reference_id != 1 :
+                assert reference == 100.0
+            else :
+                avg = sum( range(node_id, 4) ) / (4 - node_id)
+                assert reference == avg
 main()
 print('data4cov_reference: OK')
 sys.exit(0)
