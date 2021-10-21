60d741e7e6236f3887479b74f7b15162b846b3c1
Brad Bell <bradbell@seanet.com>
2021-10-21 04:06:36 -0700

    master: get_cov_info.py: add abs_covariate_id_set, rel_covariate_id_set.

--- at_cascade/get_cov_info.py
+++ at_cascade/get_cov_info.py
@@ -11,6 +11,7 @@
 {xsrst_begin get_cov_info}
 {xsrst_spell
     dict
+    rel
 }
 
 Splitting Covariate Reference Index
@@ -39,6 +40,24 @@ cov_info
 ********
 The return value *cov_info* is a `dict` with the following keys:
 
+abs_covariate_id_set
+====================
+if *key* is abs_covariate_id_set, *cov_info[key]* is a ``set`` of ``int``.
+A covariate_id is in this set if and only if the corresponding
+covariate name is in :ref:`all_option_table.absolute_covariates`.
+If absolute_covariates does not appear in the all_option table,
+*cov_info[key]* is the empty set.
+The reference value for these absolute covariates is the same
+for all nodes and all values of the splitting covariate.
+
+rel_covariate_id_set
+====================
+if *key* is rel_covariate_id_set, *cov_info[key]* is a set of ``int``.
+A covariate_id is in this set if and only if
+it is not in abs_covariate_set (see above) and it is not the
+split_covariate_id (see below).
+The reference value for these relative covariates depends on the
+node and value of the splitting covariate.
 
 Split Keys
 ==========
@@ -56,10 +75,11 @@ split_level
 if *key* is split_level, *cov_info[key]* is an ``int``
 representation of :ref:`all_option_table.split_list.split_level`.
 
-split_covariate_name
---------------------
-if *key* is split_covariate_name, *cov_info[key]* is a ``str``
-representation of :ref:`all_option_table.split_list.split_covariate_name`.
+split_covariate_id
+------------------
+if *key* is split_covariate_id, *cov_info[key]* is an ``int``
+equal to the covariate_id corresponding to the
+:ref:`all_option_table.split_list.split_covariate_name`.
 
 split_reference_list
 --------------------
@@ -73,7 +93,6 @@ if *key* is split_reference_id, *cov_info[key]* is an ``int``
 containing an index in the split_reference_list.
 The corresponding value is split_reference_list is equal to
 to reference value for split_covariate_name in the covariate table.
-abs_covariate_id_list
 
 
 {xsrst_end get_cov_info}
@@ -89,13 +108,39 @@ def get_cov_info(
 # END syntax
 ) :
     #
-    # split_list
-    split_list = None
+    # split_list, absolute_covariates
+    split_list          = None
+    absolute_covariates = None
     for row in all_option_table :
         if row['option_name'] == 'split_list' :
             split_list           = row['option_value']
+        if row['option_name'] == 'absolute_covariates' :
+            absolute_covariates  = row['option_value']
+    #
+    # abs_covariate_id_set
+    abs_covariate_id_set = set()
+    if not absolute_covariates is None :
+        absolute_covariate_list = absolute_covariates.split()
+        for covariate_name in absolute_covariate_list :
+            covariate_id = at_cascade.table_name2id(
+                covariate_table, 'covariate', covariate_name
+            )
+            abs_covariate_id_set.add(covariate_id)
+    #
+    # rel_covariate_id_set
+    rel_covariate_id_set = set()
     if split_list is None :
-        return dict()
+        for covariate_id in range( len(covariate_table) ) :
+            if not covariate_id in abs_covariate_id_set :
+                rel_covariate_id_set.add( covariate_id )
+    #
+    # case where split_list not in all_option table
+    if split_list is None :
+        cov_info = {
+            'abs_covariate_id_set':  abs_covariate_id_set,
+            'rel_covariate_id_set':  rel_covariate_id_set,
+        }
+        return cov_info
     #
     # split_level, split_covarate_name, split_reference_list
     temp_list            = split_list.split()
@@ -110,12 +155,15 @@ def get_cov_info(
         covariate_table, 'covariate', split_covariate_name
     )
     #
+    # rel_covariate_id_set
+    rel_covariate_id_set.discard( split_covariate_id )
+    #
     # split_reference
     split_reference = covariate_table[split_covariate_id]['reference']
     #
     # split_reference_id
     if not split_reference in split_reference_list :
-        msg  = 'Cannot find covaraite table value for splitting covariate '
+        msg  = 'Cannot find covariate table value for splitting covariate '
         msg += 'in split_reference_list\n'
         msg += f'split_list = {split_list}, '
         msg += f'split_covariate_id = {split_covariate_id}, '
@@ -124,9 +172,11 @@ def get_cov_info(
     split_reference_id = split_reference_list.index( split_reference )
     #
     cov_info = {
+        'abs_covariate_id_set':  abs_covariate_id_set,
+        'rel_covariate_id_set':  rel_covariate_id_set,
         'split_list':            split_list,
         'split_level':           split_level,
-        'split_covariate_name':  split_covariate_name,
+        'split_covariate_id':    split_covariate_id,
         'split_reference_list':  split_reference_list,
         'split_reference_id':    split_reference_id,
     }
