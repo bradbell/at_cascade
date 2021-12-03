c851926bdfcd848f67d57d8c325234bd0d64a3af
Brad Bell <bradbell@seanet.com>
2021-12-03 06:10:57 -0700

    master: run_parallel.py: run_parallel_job: remove recursive call.

--- at_cascade/run_parallel.py
+++ at_cascade/run_parallel.py
@@ -82,6 +82,7 @@ import at_cascade
 # ----------------------------------------------------------------------------
 shared_number_cpu_inuse = 0
 shared_job_wait_list    = list()
+shared_job_run_list     = list()
 # ----------------------------------------------------------------------------
 def run_parallel_job(
     job_table,
@@ -108,10 +109,12 @@ def run_parallel_job(
     #
     global shared_number_cpu_inuse
     global shared_job_wait_list
+    global shared_job_run_list
     #
-    # run start job
     if not skip_this_job :
-        assert master_process
+        #
+        # run_one_job
+        # do not want the lock to be aquired during this operation
         at_cascade.run_one_job(
             job_table         = job_table,
             run_job_id        = this_job_id ,
@@ -119,99 +122,146 @@ def run_parallel_job(
             node_table        = node_table,
             fit_integrand     = fit_integrand,
             trace_fit         = trace_fit,
-    )
+        )
     #
-    # start_child_job_id, end_child_job_id
-    start_child_job_id = job_table[this_job_id ]['start_child_job_id']
-    end_child_job_id   = job_table[this_job_id ]['end_child_job_id']
+    # lock
+    lock.acquire()
     #
-    if start_child_job_id < end_child_job_id :
+    # shared_job_run_list
+    if not skip_this_job :
+        index = shared_job_run_list.index( this_job_id )
+        del shared_job_run_list[index]
         #
-        # shared_job_wait_list
-        lock.acquire()
-        new_wait_list = list( range(start_child_job_id, end_child_job_id) )
-        shared_job_wait_list += new_wait_list
-        lock.release()
+    #
+    # shared_job_wait_list
+    start_child_job_id    = job_table[this_job_id ]['start_child_job_id']
+    end_child_job_id      = job_table[this_job_id ]['end_child_job_id']
+    new_wait_range        = range(start_child_job_id, end_child_job_id)
+    shared_job_wait_list += list( new_wait_range )
     #
     while True :
+        # lock
+        # lock is acquired at beginning of this loop
         #
-        # wait for lock on shared information
-        lock.acquire()
+        # event
+        # shared_job_wait_list or shared_job_run_list may have changed
+        event.set()
         #
         # n_job_wait
         n_job_wait = len(shared_job_wait_list)
         #
-        # check if nothing left to do
         if n_job_wait == 0 :
+            if len( shared_job_run_list ) == 0 :
+                #
+                # no jobs running or waiting
+                if master_process :
+                    # we are done, return to run_parallel
+                    lock.release()
+                    return
+                else :
+                    # return this processor
+                    shared_number_cpu_inuse -= 1
+                    lock.release()
+                    return
+            else :
+                #
+                # jobs are running but none are waiting
+                if master_process :
+                    #
+                    # wait for an event on another process,
+                    # then go back to the while True point above
+                    event.clear()
+                    lock.release()
+                    evant.wait()
+                    lock.acquire()
+                else :
+                    # return this processor
+                    shared_number_cpu_inuse -= 1
+                    lock.release()
+                    return
+        else :
             #
-            # we will terminate this process
-            shared_number_cpu_inuse -= 1
+            # n_cpu_spawn
+            n_cpu_available  = max_number_cpu - shared_number_cpu_inuse
+            n_cpu_spawn      = min(n_cpu_available, n_job_wait - 1)
             #
-            # done with lock on shared data
+            # shared_numper_cpu_inuse
+            shared_number_cpu_inuse += n_cpu_spawn
+            #
+            # job_run_list
+            job_run_list = shared_job_wait_list[: n_cpu_spawn + 1]
+            #
+            # shared_job_wait_list
+            shared_job_wait_list = shared_job_wait_list[n_cpu_spawn + 1 :]
+            #
+            # shared_job_run_list
+            shared_job_run_list += job_run_list
+            #
+            # lock
             lock.release()
             #
-            # terminate this process
-            return
-        #
-        # n_cpu_spawn
-        n_cpu_available  = max_number_cpu - shared_number_cpu_inuse
-        n_cpu_spawn      = min(n_cpu_available, n_job_wait - 1)
-        #
-        # shared_numper_cpu_inuse
-        shared_number_cpu_inuse += n_cpu_spawn
-        #
-        # job_run_list
-        job_run_list = shared_job_wait_list[: n_cpu_spawn + 1]
-        #
-        # shared_job_wait_list
-        shared_job_wait_list = shared_job_wait_list[n_cpu_spawn + 1 :]
-        #
-        # done with lock on shared data
-        lock.release()
-        #
-        # skip_child_job
-        skip_child_job = False
-        #
-        # is_child_master_process
-        is_child_master_process = False
-        #
-        # spawn the new processes
-        for i in range(n_cpu_spawn) :
+            # event
+            # shared_job_wait_list or shared_job_run_list may have changed
+            event.set()
             #
-            # p
-            args = (
-                job_table,
-                job_run_list[i] ,
-                all_node_database,
-                node_table,
-                fit_integrand,
-                trace_fit,
-                skip_child_job,
-                max_number_cpu,
-                is_child_master_process,
-                lock,
-                event,
+            # skip_child_job
+            skip_child_job = False
+            #
+            # is_child_master_process
+            is_child_master_process = False
+            #
+            # spawn the new processes
+            for i in range(n_cpu_spawn) :
+                #
+                # p
+                args = (
+                    job_table,
+                    job_run_list[i] ,
+                    all_node_database,
+                    node_table,
+                    fit_integrand,
+                    trace_fit,
+                    skip_child_job,
+                    max_number_cpu,
+                    is_child_master_process,
+                    lock,
+                    event,
+                )
+                target = run_parallel_job
+                p = multiprocessing.Process(target = target, args = args)
+                #
+                p.deamon = False
+                p.start()
+            #
+            # job_id
+            job_id = job_run_list[n_cpu_spawn]
+            #
+            # run_one_job
+            # do not want the lock to be aquired during this operation
+            at_cascade.run_one_job(
+                job_table         = job_table,
+                run_job_id        = job_id,
+                all_node_database = all_node_database,
+                node_table        = node_table,
+                fit_integrand     = fit_integrand,
+                trace_fit         = trace_fit,
             )
-            target = run_parallel_job
-            p = multiprocessing.Process(target = target, args = args)
             #
-            p.deamon = True
-            p.start()
+            # lock
+            lock.acquire()
+            #
+            # shared_job_run_list
+            index = shared_job_run_list.index( job_id )
+            del shared_job_run_list[index]
+            #
+            # shared_job_wait_list
+            start_child_job_id    = job_table[job_id]['start_child_job_id']
+            end_child_job_id      = job_table[job_id]['end_child_job_id']
+            new_wait_range        = range(start_child_job_id, end_child_job_id)
+            shared_job_wait_list += list( new_wait_range )
         #
-        # run the last child job with the current process
-        run_parallel_job(
-            job_table,
-            job_run_list[n_cpu_spawn] ,
-            all_node_database,
-            node_table,
-            fit_integrand,
-            trace_fit,
-            skip_child_job,
-            max_number_cpu,
-            master_process,
-            lock,
-            event,
-        )
+        # lock
+        # lock is acquired at end of this loop
 # ----------------------------------------------------------------------------
 def run_parallel(
 # BEGIN syntax
@@ -229,6 +279,7 @@ def run_parallel(
 ) :
     global shared_number_cpu_inuse
     global shared_job_wait_list
+    global shared_job_run_list
     #
     assert job_table         is not None
     assert start_job_id      is not None
@@ -245,6 +296,12 @@ def run_parallel(
     # shared_job_wait_list
     shared_job_wait_list = list()
     #
+    # shared_job_run_list
+    if skip_start_job :
+        shared_job_run_list = list()
+    else :
+        shared_job_run_list = [ start_job_id ]
+    #
     # master_process
     master_process = True
     #
@@ -253,6 +310,7 @@ def run_parallel(
     #
     # event
     event = multiprocessing.Event()
+    event.set()
     #
     # run_parallel_job
     run_parallel_job(
