05599e0658b40aca49e98b3c7872d0c756651adc
Brad Bell <bradbell@seanet.com>
2021-12-28 03:36:45 -0700

    master: Advance to at_cascade-2021.12.28

--- at_cascade/ihme/__init__.py
+++ at_cascade/ihme/__init__.py
@@ -1,4 +1,4 @@
-# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 # at_cascade: Cascading Dismod_at Analysis From Parent To Child Regions
 #           Copyright (C) 2021-21 University of Washington
 #              (Bradley M. Bell bradbell@uw.edu)
@@ -6,7 +6,7 @@
 # This program is distributed under the terms of the
 #     GNU Affero General Public License version 3.0 or later
 # see http://www.gnu.org/licenses/agpl.txt
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 #
 # input files for all diseases
 location_inp_file  = 'ihme_db/DisMod_AT/metadata/gbd2019_location_map.csv'
