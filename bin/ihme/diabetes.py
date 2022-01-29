d5b12b49771610f18f371130dbd6d3aba436a3d8
Brad Bell <bradbell@seanet.com>
2022-01-29 06:09:30 -0700

    master: diabetes.py: settings for 01.29 fit.

--- bin/ihme/diabetes.py
+++ bin/ihme/diabetes.py
@@ -111,12 +111,12 @@ rate_case = 'iota_pos_rho_zero'
 # zero_sum_child_rate
 # space separate list of rates. The child random effects for each of these
 # rates will be constrained to sum to zero. Use empty string for none.
-zero_sum_child_rate = 'iota chi'
+zero_sum_child_rate = ''
 #
 # ode_step_size
 # step size to use (in age and time) when approximationg intergals and
 # solutions of the ODE.
-ode_step_size = 10.0
+ode_step_size = 5.0
 #
 # age_avg_split_list
 # An extra list of ages at which to split the interal and ODE approximation.
