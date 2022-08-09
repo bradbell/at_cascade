2a94acca56ed0d044f540fa1f8aa3318cc061ff9
Brad Bell <bradbell@seanet.com>
2022-08-09 03:35:54 -0700

    master: run_sphinx.sh: adapt to new version of xrst.
    keyword: improve this file.

--- bin/run_sphinx.sh
+++ bin/run_sphinx.sh
@@ -31,62 +31,30 @@ then
     ok='no'
 elif [ "$2" == '0' ]
 then
-    line_increment=''
+    error_line=''
 else
-    line_increment="$2"
+    error_line="-e $2"
 fi
 if [ "$ok" == 'no' ]
 then
-    echo 'usage: bin/run_sphinx.sh target line_increment'
+    echo 'usage: bin/run_sphinx.sh target error_line'
     echo 'target is html or pdf'
-    echo 'line_increment = 0 for no line number table'
+    echo 'error_line = 0 for no line number table'
     exit 1
 fi
 # -----------------------------------------------------------------------------
-echo "xrst $target doc.xrst sphinx $line_increment"
-if ! xrst $target doc.xrst sphinx $line_increment >& run_sphinx.$$
+echo "xrst doc.xrst -t $target -o doc $error_line"
+if ! xrst doc.xrst -t $target $error_line -o doc >& >( tee run_sphinx.$$ )
 then
-    cat run_sphinx.$$
     echo 'bin/run_sphinx: aboring due to xrst errors above'
     rm run_sphinx.$$
     exit 1
 fi
 if grep '^warning:' run_sphinx.$$ > /dev/null
 then
-    cat run_sphinx.$$
     echo 'bin/run_sphinx: aboring due to xrst warnings above'
     rm run_sphinx.$$
     exit 1
-fi
-# -----------------------------------------------------------------------------
-if [ "$target" == 'html' ]
-then
-    sphinx_target='html'
-    destination='doc'
-else
-    sphinx_target='latex'
-    destination='doc/latex'
-fi
-echo "sphinx-build -b $sphinx_target sphinx $destination"
-if ! sphinx-build -b $sphinx_target sphinx $destination >& run_sphinx.$$
-then
-    cat run_sphinx.$$
-    echo 'bin/run_sphinx: aboring due to sphinx warnings above'
-    rm run_sphinx.$$
-    exit 1
-fi
-if grep '^build succeeded,.*warning' run_sphinx.$$ > /dev/null
-then
-    cat run_sphinx.$$
-    echo 'bin/run_sphinx: aboring due to sphinx warnings above'
-    rm run_sphinx.$$
-    exit 1
-fi
-# -----------------------------------------------------------------------------
-if [ "$sphinx_target" == 'latex' ]
-then
-    echo_eval cd doc/latex
-    make at_cascade.pdf
 fi
 # -----------------------------------------------------------------------------
 rm run_sphinx.$$
