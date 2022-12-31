52b5b0010b5d21f5574d08a4eb9b8df7f62a5ca0
Brad Bell <bradbell@seanet.com>
2022-12-31 14:56:25 -0700

    master: add .readthedocs.yaml.

--- bin/devel.sh
+++ bin/devel.sh
@@ -22,6 +22,7 @@ version_files='
 # Names that begin with a / are relative to top source directroy.
 # All other names are relavie paths somewhere below the top source directory.
 ignore_files='
+   /.readthedocs.yaml
    /.gitignore
    /batch_edit.sed
    /bin/devel.sh
