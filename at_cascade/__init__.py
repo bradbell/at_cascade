1fa444d710a5eafdb3a946bc1e5af7838150526c
Brad Bell <bradbell@seanet.com>
2024-02-20 12:11:44 -0700

    master: Advance to at_cascade-2024.2.20

--- at_cascade/__init__.py
+++ at_cascade/__init__.py
@@ -15,7 +15,7 @@ at_cascade.version
 ******************
 The version number for this copy of at_cascade.
 {xrst_code py}'''
-version = '2024.2.9'
+version = '2024.2.20'
 '''{xrst_code}
 
 at_cascade.constant_table_list
