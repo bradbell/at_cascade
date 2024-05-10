5fa0606da149a81a81c07d154485d8c686b50d73
Brad Bell <bradbell@seanet.com>
2024-05-10 05:03:18 -0700

    master: coverage.py: add documentation.

--- example/csv/coverage.py
+++ example/csv/coverage.py
@@ -2,34 +2,129 @@
 # SPDX-FileCopyrightText: University of Washington <https://www.washington.edu>
 # SPDX-FileContributor: 2021-24 Bradley M. Bell
 # ----------------------------------------------------------------------------
-# imports
+r'''
+{xrst_begin csv.coverage}
+{xrst_spell
+   cv
+   sincidence
+   std
+   meas
+   random random
+}
+
+Csv Example Checking Coverage For Asymptotic Samples
+####################################################
+In order to keep this example simple, and fast to execute:
+
+#. The only model variable is iota and it is constant
+   with respect to age and time; see no_effect_rate.csv.
+
+#. The only integrand is Sincidence; see predict_integrand.csv below.
+
+#. The only node (location) is n0 and only the 'both' sex value is fit
+   for this node; see node.csv and refit_split in option_fit.csv below.
+
+
+Age-Time Grid
+*************
+These are the age and time points that appear in the
+:ref:`csv.simulate@Input Files@covariate.csv` file
+and the prediction files; see
+:ref:`csv.predict@Output Files@fit_predict.csv@age` and time in csv.predict.
+{xrst_code py}'''
+age_grid   = [ 0.0,  100.0]
+time_grid  = [ 1980, 2020]
+'''{xrst_code}
+Note that Sincidence is predicted for the different
+age and time pairs (but do not depend on age or time for this example).
+
+Truth
+*****
+These are the true value for iota and omega; i.e., the values
+used by :ref:`csv.simulate-name` when it simulates the data:
+{xrst_code py}'''
+true_iota  = 0.01
+true_omega = 0.02
+'''{xrst_code}
+The value of omega is specified in covariate.csv below and
+constrained to this value during :ref:`csv.fit-name`
+The value of iota is estimated by maximizing the likelihood of the data.
+
+Random Seed
+***********
+This is the random seed for this example.
+Seeding the random number generator is made complicated by the fact that
+this example uses multiple calls to :ref:`csv.simulate-name` and
+:ref:`csv.fit-name` and each call needs to use a different random seed.
+This is done in a way so that if the random seed below is the same,
+the result for this example will be reproduced.
+{xrst_code py}'''
+import time
+import random
+random_seed = int( time.time() )
+random.seed( random_seed )
+'''{xrst_code}
+Note that the choice above for the random seed changes each time this
+example is run.
+
+Number Data Per Fit
+*******************
+This is the number of data points in each data set; i.e., each fit.
+{xrst_code py}'''
+n_data_per_fit  = 5
+'''{xrst_code}
+
+Number Samples Per Fit
+**********************
+This is the number of samples from the asymptotic distribution
+that generated for each fit.
+This is also the number of times each prediction is repeated.
+{xrst_code py}'''
+n_sample_per_fit = 50
+'''{xrst_code}
+The more samples per fit, the more accurate the standard deviation
+that approximates the posterior statistics.
+
+Number of Fits
+**************
+This is the number of times a data set is simulated and fit.
+The larger this number, the more accurate sample probability that
+the fit will be withing the confidence limits.
+{xrst_code py}'''
+number_fit = 40
+'''{xrst_code}
+The more samples per fit, the more accurate the standard deviation.
+The time to run this example divided by *number_fit* should be nearly constant.
+
+Measurement Noise CV
+********************
+This the coefficient of variation for the Gaussian measurement noise; i.e.,
+the measurement standard deviation is the measurement mean times this value.
+{xrst_code py}'''
+meas_std_cv = 0.5
+'''{xrst_code}
+
+Source Code
+***********
+{xrst_literal
+   # BEGIN PYTHON
+   # END PYTHON
+}
+
+{xrst_end csv.coverage}
+------------------------------------------------------------------------------
+'''
+# BEGIN PYTHON
 import os
 import sys
-import time
 import shutil
 import numpy
-import random
 import scipy
 # import at_cascade with a preference current directory version
 current_directory = os.getcwd()
 if os.path.isfile( current_directory + '/at_cascade/__init__.py' ) :
    sys.path.insert(0, current_directory)
 import at_cascade
-# ----------------------------------------------------------------------------
-#
-age_grid   = [ 0.0,  100.0]
-time_grid  = [ 1980, 2020]
-#
-true_iota  = 0.01
-true_omega = 0.02
-#
-random_seed = str( int( time.time() ) )
-random.seed(random_seed)
-#
-number_data         = 5
-number_sample       = 50
-meas_std_cv         = 0.5
-number_data_set     = 40
 # ----------------------------------------------------------------------------
 # simulation files
 # ----------------------------------------------------------------------------
@@ -38,12 +133,13 @@ number_data_set     = 40
 sim_file = dict()
 #
 # option_sim.csv
+# The random_seed will get replaced by the next_random_seed function below.
 sim_file['option_sim.csv'] = \
 '''name,value
 absolute_tolerance,1e-10
 float_precision,12
+random_seed,0
 '''
-sim_file['option_sim.csv'] += f'random_seed,{random_seed}\n'
 #
 # node.csv
 sim_file['node.csv'] = \
@@ -74,7 +170,7 @@ header += 'time_lower,time_upper,meas_std_cv,meas_std_min\n'
 row    += f'2000,2000,{meas_std_cv},0.0\n'
 #
 sim_file['simulate.csv'] = header
-for simulate_id in range( number_data ) :
+for simulate_id in range( n_data_per_fit ) :
    sim_file['simulate.csv'] += f'{simulate_id},' + row
 # ----------------------------------------------------------------------------
 # fit files
@@ -84,6 +180,7 @@ for simulate_id in range( number_data ) :
 fit_file = dict()
 #
 # option_fit.csv
+# The random_seed will get replaced by the next_random_seed function below.
 fit_file['option_fit.csv']  =  \
 '''name,value
 refit_split,false
@@ -91,8 +188,8 @@ quasi_fixed,false
 tolerance_fixed,1e-8
 max_num_iter_fixed,50
 '''
-fit_file['option_fit.csv'] += f'number_sample,{number_sample}\n'
-fit_file['option_fit.csv'] += f'random_seed,{random_seed}\n'
+fit_file['option_fit.csv'] += f'number_sample,{n_sample_per_fit}\n'
+fit_file['option_fit.csv'] += 'random_seed,0\n'
 #
 # option_predict.csv
 fit_file['option_predict.csv']  =  \
@@ -147,9 +244,9 @@ def next_random_seed(option_file) :
          row['value'] = str(new_seed)
    assert found
    at_cascade.csv.write_table(option_file, option_table)
-#
 # -----------------------------------------------------------------------------
 # sim
+# Simulate the data for one fit
 def sim(sim_dir) :
    #
    # write input csv files
@@ -174,6 +271,7 @@ def sim(sim_dir) :
    )
 # -----------------------------------------------------------------------------
 # fit
+# Do one fit
 def fit(sim_dir, fit_dir) :
    #
    # node.csv, covarite.csv
@@ -242,10 +340,6 @@ def weighted_residual(sim_dir, fit_dir) :
    # n_grid
    n_grid = len(age_grid) * len(time_grid)
    #
-   # n_integrand
-   # number of integrands in predict_integrand.csv
-   n_integrand = 2
-   #
    # prefix
    for prefix in [ 'tru', 'fit', 'sam' ] :
       #
@@ -254,7 +348,7 @@ def weighted_residual(sim_dir, fit_dir) :
          file_name = f'{fit_dir}/{prefix}_predict.csv'
       )
       if prefix == 'sam' :
-         assert len(table) == number_sample * n_grid
+         assert len(table) == n_sample_per_fit * n_grid
       else :
          assert len(table) == n_grid
       #
@@ -272,18 +366,27 @@ def weighted_residual(sim_dir, fit_dir) :
       summary_dict['std'] = list()
       for i_grid in range(n_grid) :
          sumsq = 0.0
-         for i_sample in range( number_sample ) :
-            sample  = summary_dict['sam'][ i_grid * number_sample + i_sample ]
-            diff    = sample - summary_dict['fit'][i_grid]
+         for i_sample in range( n_sample_per_fit ) :
+            sample = summary_dict['sam'][i_grid * n_sample_per_fit + i_sample]
+            diff   = sample - summary_dict['fit'][i_grid]
             sumsq  += diff * diff
          #
-         summary_dict['std'].append( numpy.sqrt( sumsq / number_sample ) )
+         summary_dict['std'].append( numpy.sqrt( sumsq / n_sample_per_fit ) )
+   #
+   # eps99
+   eps99 = 99.0 * numpy.finfo(float).eps
+   #
+   # tru
+   # Check that the truth for Sincidence is equal to true_iota.
+   for tru in summary_dict['tru'] :
+      assert abs( 1.0 -  tru / true_iota) < eps99
    #
    # fit
-   eps99 = 99.0 * numpy.finfo(float).eps
+   # Check that predicitons for Sincidence do not depend on or time.
+   # This might not be true for other integerands; e.g., prevalence.
    fit    = summary_dict['fit'][0]
    for other_fit in summary_dict['fit'] :
-      assert abs( 1.0 -  other_fit/ fit) < eps99
+      assert abs( 1.0 -  other_fit / fit) < eps99
    #
    # residual
    tru      = summary_dict['tru'][0]
@@ -305,7 +408,7 @@ def main() :
    #
    # residual
    residual_list = list()
-   for i_data_set in range(number_data_set) :
+   for i_data_set in range(number_fit) :
       #
       # fit_dir/n0
       at_cascade.empty_directory( f'{fit_dir}/n0' )
@@ -331,15 +434,17 @@ def main() :
    for residual in residual_list :
       if abs(residual) <= 0.68 :
          count += 1
-   confidence_sample = count / number_data_set
+   confidence_sample = count / number_fit
    difference = confidence_sample - confidence_theory
-   msg  = f'theory = {confidence_theory}, '
-   msg += f'sample = {confidence_sample}, '
-   msg += f'difference = {difference}'
+   msg  = f'random_seed = {random_seed}, '
+   msg += f'theory = {confidence_theory:.4f}, '
+   msg += f'sample = {confidence_sample:.4f}, '
+   msg += f'difference = {difference:.4f}'
    print( msg )
    assert abs(difference) < 0.1
    #
    print('csv.coverage: OK')
    sys.exit(0)
-# END_PYTHON
+# -----------------------------------------------------------------------------
 main()
+# END PYTHON
