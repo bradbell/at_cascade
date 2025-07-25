28885588969ac7b9cd72c26af956935962fcbf2b
Garland Culbreth <culbreth.garland@gmail.com>
2025-07-25 00:04:01 +0000

    number_sample_predict: fix broken xrst crossref

--- at_cascade/csv/pre_one_job.py
+++ at_cascade/csv/pre_one_job.py
@@ -89,8 +89,8 @@ number_sample_predict
 *********************
 This integer option specifies the number of samples generated for each
 prediction. This value is specified in
-:ref:`csv.predict#Input Files@option_predict.csv@number_sample_predict` If not
-set, :ref:`csv.fit@Input Files@option_fit.csv@number_sample` from
+:ref:`csv.predict@Input Files@option_predict.csv@number_sample_predict`. The
+default value is :ref:`csv.fit@Input Files@option_fit.csv@number_sample` from
 :ref:`csv.fit@Input Files@option_fit.csv` will be used.
 
 Csv Output Files
